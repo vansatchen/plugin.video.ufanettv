5dba9568dc9cb6323c8801426299fb5aac4a4cf8
vansatchen <rusatch@gmail.com>
2022-01-21 23:40:35 +0500

    publishing plugin version 0.2.0

---
+++ main.py
@@ -0,0 +1,321 @@
+import sys
+import os
+from urllib.parse import urlencode, parse_qsl
+import xbmcgui, xbmcplugin, xbmcaddon, xbmcvfs
+import requests
+from resources.functions import *
+
+# Get the plugin url in plugin:// notation.
+_url = sys.argv[0]
+# Get the plugin handle as an integer number.
+_handle = int(sys.argv[1])
+#addonPath = xbmcvfs.translatePath(xbmcaddon.Addon(id='plugin.video.ufanettv').getAddonInfo('path'))
+# Check that login and password available
+checkSettings(_handle)
+# Get deviceID
+deviceID = checkDeviceID(_handle)
+
+# Get token
+getToken = requests.post('http://api.ufanet.platform24.tv/v2/auth/device', json={"device_id": deviceID})
+for el in getToken.json():
+    if el == 'access_token':
+        accessToken = getToken.json().get('access_token')
+    if el == 'error_code':
+        file = open(profilePath + 'DeviceID.txt', 'w')
+        file.write('')
+        file.close()
+        deviceID = checkDeviceID(_handle)
+
+# Get channels
+paramsChan = {"includes": "images.whiteback", "access_token": accessToken}
+getChannels = requests.get('http://api.ufanet.platform24.tv/v2/channels/categories', params=paramsChan)
+
+TVCHANNELS = {}
+for cat in getChannels.json():
+    TVCHANNELS[cat['name']] = channelFunc(cat['channels'])
+
+
+# Get films and serials
+paramsFilms = {'access_token': accessToken, 'limit': '100'}
+getFilms = requests.get('http://api.ufanet.platform24.tv/v2/programs', params=paramsFilms)
+
+FILMS = {}
+for film in getFilms.json():
+    FILMS[film['title']] = filmFunc(film)
+
+
+CLASSES = {}
+CLASSES['ТВ каналы'] = [TVCHANNELS]
+CLASSES['Фильмы и сериалы'] = [FILMS]
+
+def get_url(**kwargs):
+    return '{0}?{1}'.format(_url, urlencode(kwargs))
+
+def get_classes():
+    return CLASSES.keys()
+
+def get_categories():
+    return TVCHANNELS.keys()
+
+def get_videos(category):
+    return TVCHANNELS[category]
+
+def get_films():
+    return FILMS.keys()
+
+def get_views(film):
+    paramsView = {'access_token': accessToken}
+    getView = requests.get('http://api.ufanet.platform24.tv/v2/programs/' + film + '/schedule', params=paramsView)
+    return getView.json()
+
+
+def list_classes():
+    xbmcplugin.setPluginCategory(_handle, 'classes')
+    xbmcplugin.setContent(_handle, 'videos')
+    classes = get_classes()
+    for classe in classes:
+        list_item = xbmcgui.ListItem(label=classe)
+        if classe == 'ТВ каналы': url = get_url(action='listTvChannels', classe=classe)
+        elif classe == 'Фильмы и сериалы': url = get_url(action='listFilms', classe=classe)
+#        else: url = get_url(action='listTvChannels', classe=classe)
+        is_folder = True
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def listFilms():
+    xbmcplugin.setPluginCategory(_handle, 'Films')
+    xbmcplugin.setContent(_handle, 'videos')
+    # Get video categories
+    films = get_films()
+    # Iterate through categories
+    for film in films:
+        list_item = xbmcgui.ListItem(label=film)
+        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+        # Here we use the same image for all items for simplicity's sake.
+        # In a real-life plugin you need to set each image accordingly.
+        list_item.setArt({'thumb': FILMS[film][0]['thumb'],
+                          'icon': FILMS[film][0]['thumb'],
+                          'fanart': FILMS[film][0]['thumb']})
+        # Set additional info for the list item.
+        # Here we use a category name for both properties for for simplicity's sake.
+        # setInfo allows to set various information for an item.
+        # For available properties see the following link:
+        # https://codedocs.xyz/xbmc/xbmc/group__python__xbmcgui__listitem.html#ga0b71166869bda87ad744942888fb5f14
+        # 'mediatype' is needed for a skin to display info for this ListItem correctly.
+        list_item.setInfo('video', {'title': film,
+                                    'genre': film,
+                                    'mediatype': 'video'})
+        # Create a URL for a plugin recursive call.
+        # Example: plugin://plugin.video.example/?action=listing&category=Animals
+        url = get_url(action='views', film=FILMS[film][0]['id'])
+        # is_folder = True means that this item opens a sub-list of lower level items.
+        is_folder = True
+        # Add our item to the Kodi virtual folder listing.
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def listTvChannels():
+    xbmcplugin.setPluginCategory(_handle, 'Categories')
+    xbmcplugin.setContent(_handle, 'videos')
+    # Get video categories
+    categories = get_categories()
+    # Iterate through categories
+    for category in categories:
+        list_item = xbmcgui.ListItem(label=category)
+        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+        # Here we use the same image for all items for simplicity's sake.
+        # In a real-life plugin you need to set each image accordingly.
+        list_item.setArt({'thumb': TVCHANNELS[category][0]['thumb'],
+                          'icon': TVCHANNELS[category][0]['thumb'],
+                          'fanart': TVCHANNELS[category][0]['thumb']})
+        # Set additional info for the list item.
+        # Here we use a category name for both properties for for simplicity's sake.
+        # setInfo allows to set various information for an item.
+        # For available properties see the following link:
+        # https://codedocs.xyz/xbmc/xbmc/group__python__xbmcgui__listitem.html#ga0b71166869bda87ad744942888fb5f14
+        # 'mediatype' is needed for a skin to display info for this ListItem correctly.
+        list_item.setInfo('video', {'title': category,
+                                    'genre': category,
+                                    'mediatype': 'video'})
+        # Create a URL for a plugin recursive call.
+        # Example: plugin://plugin.video.example/?action=listing&category=Animals
+        url = get_url(action='listing', category=category)
+        # is_folder = True means that this item opens a sub-list of lower level items.
+        is_folder = True
+        # Add our item to the Kodi virtual folder listing.
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+#    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def listViews(film):
+    # Set plugin category. It is displayed in some skins as the name
+    # of the current section.
+    xbmcplugin.setPluginCategory(_handle, film)
+    # Set plugin content. It allows Kodi to select appropriate views
+    # for this type of content.
+    xbmcplugin.setContent(_handle, 'videos')
+    # Get the list of videos in the category.
+    videos = get_views(film)
+    # Iterate through videos.
+    for video in videos:
+        # Create a list item with a text label and a thumbnail image.
+        list_item = xbmcgui.ListItem(label=video['channel']['name'])
+        # Set additional info for the list item.
+        # 'mediatype' is needed for skin to display info for this ListItem correctly.
+        if video['episode'] is None:
+            episode = ""
+        else:
+            episode = 'S' + str(video['episode']['season']) + 'E' + str(video['episode']['series']) + ' ' + str(video['episode']['title'])
+        list_item.setInfo('video', {'title': video['channel']['name'] + " " + episode,
+                                    'genre': str(video['program']['category']['name']),
+                                    'mediatype': 'video'})
+        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+        # Here we use the same image for all items for simplicity's sake.
+        # In a real-life plugin you need to set each image accordingly.
+        list_item.setArt({'thumb': video['channel']['icon'], 'icon': video['channel']['icon'], 'fanart': video['channel']['icon']})
+        # Set 'IsPlayable' property to 'true'.
+        # This is mandatory for playable items!
+        list_item.setProperty('IsPlayable', 'true')
+        # Create a URL for a plugin recursive call.
+        # Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4
+        url = get_url(action='playView', video=str(video['channel_id']) + " " + str(video['timestamp']))
+        # Add the list item to a virtual Kodi folder.
+        # is_folder = False means that this item won't open any sub-list.
+        is_folder = False
+        # Add our item to the Kodi virtual folder listing.
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+#    listitem = xbmcgui.ListItem(label='Next page')
+#    listitem.setProperty('SpecialSort', 'bottom')
+#    xbmcplugin.addDirectoryItem(_handle, url, listitem, is_folder)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def listTvVideos(category):
+    # Set plugin category. It is displayed in some skins as the name
+    # of the current section.
+    xbmcplugin.setPluginCategory(_handle, category)
+    # Set plugin content. It allows Kodi to select appropriate views
+    # for this type of content.
+    xbmcplugin.setContent(_handle, 'videos')
+    # Get the list of videos in the category.
+    videos = get_videos(category)
+    # Iterate through videos.
+    for video in videos:
+        # Create a list item with a text label and a thumbnail image.
+        list_item = xbmcgui.ListItem(label=video['name'])
+        # Set additional info for the list item.
+        # 'mediatype' is needed for skin to display info for this ListItem correctly.
+        list_item.setInfo('video', {'title': video['name'],
+                                    'genre': video['genre'],
+                                    'mediatype': 'video'})
+        # Set graphics (thumbnail, fanart, banner, poster, landscape etc.) for the list item.
+        # Here we use the same image for all items for simplicity's sake.
+        # In a real-life plugin you need to set each image accordingly.
+        list_item.setArt({'thumb': video['thumb'], 'icon': video['thumb'], 'fanart': video['thumb']})
+        # Set 'IsPlayable' property to 'true'.
+        # This is mandatory for playable items!
+        list_item.setProperty('IsPlayable', 'true')
+        # Create a URL for a plugin recursive call.
+        # Example: plugin://plugin.video.example/?action=play&video=http://www.vidsplay.com/wp-content/uploads/2017/04/crab.mp4
+        url = get_url(action='play', video=video['video'])
+        # Add the list item to a virtual Kodi folder.
+        # is_folder = False means that this item won't open any sub-list.
+        is_folder = False
+        # Add our item to the Kodi virtual folder listing.
+        xbmcplugin.addDirectoryItem(_handle, url, list_item, is_folder)
+    # Add a sort method for the virtual folder items (alphabetically, ignore articles)
+    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+#    listitem = xbmcgui.ListItem(label='Next page')
+#    listitem.setProperty('SpecialSort', 'bottom')
+#    xbmcplugin.addDirectoryItem(_handle, url, listitem, is_folder)
+    # Finish creating a virtual folder.
+    xbmcplugin.endOfDirectory(_handle)
+
+
+def play_view(path):
+    paramsChan = {"access_token": accessToken, "ts": path.split()[1]}
+    urlJson = requests.get('http://api.ufanet.platform24.tv/v2/channels/' + path.split()[0] + '/stream', params=paramsChan)
+
+    try:
+        if urlJson.json()['error']:
+            xbmcgui.Dialog().ok('Внимание!', str(urlJson.json()['error']['message']))
+            return
+    except:
+        url = urlJson.json()['hls']
+
+    # Create a playable item with a path to play.
+    play_item = xbmcgui.ListItem(path=url)
+    # Pass the item to the Kodi player.
+    xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
+
+
+def play_video(path):
+    paramsChan = {"access_token": accessToken, "ts": "0"}
+    urlJson = requests.get('http://api.ufanet.platform24.tv/v2/channels/' + path + '/stream', params=paramsChan)
+
+    try:
+        if urlJson.json()['error']:
+            xbmcgui.Dialog().ok('Внимание!', str(urlJson.json()['error']['message']))
+            return
+    except:
+        url = urlJson.json()['hls']
+
+    # Create a playable item with a path to play.
+    play_item = xbmcgui.ListItem(path=url)
+    # Pass the item to the Kodi player.
+    xbmcplugin.setResolvedUrl(_handle, True, listitem=play_item)
+
+
+def router(paramstring):
+    """
+    Router function that calls other functions
+    depending on the provided paramstring
+
+    :param paramstring: URL encoded plugin paramstring
+    :type paramstring: str
+    """
+    # Parse a URL-encoded paramstring to the dictionary of
+    # {<parameter>: <value>} elements
+    params = dict(parse_qsl(paramstring))
+    # Check the parameters passed to the plugin
+    if params:
+        if params['action'] == 'listing':
+            # Display the list of videos in a provided category.
+            listTvVideos(params['category'])
+        elif params['action'] == 'play':
+            # Play a video from a provided URL.
+            play_video(params['video'])
+        elif params['action'] == 'listTvChannels':
+            listTvChannels()
+        elif params['action'] == 'listFilms':
+            listFilms()
+        elif params['action'] == 'views':
+            listViews(params['film'])
+        elif params['action'] == 'playView':
+            play_view(params['video'])
+        else:
+            # If the provided paramstring does not contain a supported action
+            # we raise an exception. This helps to catch coding errors,
+            # e.g. typos in action names.
+            raise ValueError('Invalid paramstring: {0}!'.format(paramstring))
+    else:
+        # If the plugin is called from Kodi UI without any parameters,
+        # display the list of video categories
+        list_classes()
+
+if __name__ == '__main__':
+    # Call the router function and pass the plugin call parameters to it.
+    # We use string slicing to trim the leading '?' from the plugin call paramstring
+    router(sys.argv[2][1:])
---
+++ resources/functions.py
@@ -0,0 +1,133 @@
+import sys
+import xbmcgui, xbmcplugin, xbmcaddon, xbmcvfs, xbmc
+import uuid
+import requests
+
+#addonPath = xbmcvfs.translatePath(xbmcaddon.Addon(id='plugin.video.ufanettv').getAddonInfo('path'))
+profilePath = xbmcvfs.translatePath(xbmcaddon.Addon(id='plugin.video.ufanettv').getAddonInfo('profile'))
+
+def checkSettings(_handle):
+    # Get credentials from settings
+    contractLogin = xbmcplugin.getSetting(_handle, "loginStr")
+    contractPassword = xbmcplugin.getSetting(_handle, "passStr")
+
+    if not contractLogin or not contractPassword:
+        # if settings empty, show warning and settings window
+        xbmcgui.Dialog().notification('Ошибка авторизации', 'Введите корректные данные.', xbmcgui.NOTIFICATION_ERROR)
+        xbmcaddon.Addon().openSettings()
+        sys.exit()
+
+
+def generateUUID(_handle):
+    deviceID = str(uuid.uuid4())
+    getToken = requests.post('http://api.ufanet.platform24.tv/v2/auth/device', json={"device_id": deviceID})
+    for el in getToken.json():
+        if el == 'error_code':
+            contractLogin = xbmcplugin.getSetting(_handle, "loginStr")
+            contractPassword = xbmcplugin.getSetting(_handle, "passStr")
+            getToken = requests.post('http://api.ufanet.platform24.tv/v2/auth/login', json={"login": contractLogin, "password": contractPassword})
+            try:
+                if getToken.json()['error']:
+                    xbmcgui.Dialog().ok('Внимание!', str(getToken.json()['error']['message']))
+                    sys.exit()
+            except:
+                pass
+            # Get token
+            accessToken = getToken.json()['access_token']
+            # Register device
+            jsonData = {'vendor': xbmc.getInfoLabel('System.FriendlyName'),
+                        'model': xbmc.getInfoLabel('System.BuildVersion').split()[0],
+                        'serial': deviceID,
+                        'device_type': 'tv',
+                        'version': xbmc.getInfoLabel('System.AddonVersion("plugin.video.ufanettv")'),
+                        'application_type': 'android-mobile'}
+            postDevice = requests.post('http://api.ufanet.platform24.tv/v2/users/self/devices?access_token=' + accessToken, json=jsonData)
+            # Get new devicesID
+            try:
+                if postDevice.json()['error']:
+                    xbmcgui.Dialog().ok('Внимание!', str(postDevice.json()['error']['message']))
+                    paramsChan = {"access_token": accessToken}
+                    getDevices = requests.get('http://api.ufanet.platform24.tv/v2/users/self/devices', params=paramsChan)
+                    devices = []
+                    devicesLst = []
+                    indexInt = 0
+                    for elem in getDevices.json():
+                        index = ['index', indexInt]
+                        id = ['id', elem['id']]
+                        deviceType = ['deviceType', elem['device_type']]
+                        vendor = ['vendor', elem['vendor']]
+                        model = ['model', elem['model']]
+                        loginAt = ['login_at', elem['login_at']]
+                        device = [index, id, deviceType, vendor, model, loginAt]
+                        devices.append(dict(device))
+                        indexInt += 1
+                    for elm in devices:
+                        device = str(elm['deviceType']) + ": " + str(elm['vendor']) + " - " + str(elm['model']) + "  " + str(elm['login_at'])
+                        devicesLst.append(device)
+
+                    removeDev = xbmcgui.Dialog().select('Выберите устройство для удаления', devicesLst)
+                    for device in devices:
+                        if device['index'] == int(removeDev):
+                            requests.delete('http://api.ufanet.platform24.tv/v2/users/self/devices/' + device['id'], params=paramsChan)
+                            contractLogin = xbmcplugin.getSetting(_handle, "loginStr")
+                            contractPassword = xbmcplugin.getSetting(_handle, "passStr")
+                            getToken = requests.post('http://api.ufanet.platform24.tv/v2/auth/login', json={"login": contractLogin, "password": contractPassword})
+                            # Get token
+                            accessToken = getToken.json()['access_token']
+                            # Register device
+                            jsonData = {'vendor': xbmc.getInfoLabel('System.FriendlyName'),
+                                        'model': xbmc.getInfoLabel('System.BuildVersion').split()[0],
+                                        'serial': deviceID,
+                                        'device_type': 'tv',
+                                        'version': xbmc.getInfoLabel('System.AddonVersion("plugin.video.ufanettv")'),
+                                        'application_type': 'android-mobile'}
+                            postDevice = requests.post('http://api.ufanet.platform24.tv/v2/users/self/devices?access_token=' + accessToken, json=jsonData)
+                            deviceID = postDevice.json()['id']
+            except:
+                deviceID = postDevice.json()['id']
+
+    file = open(profilePath + 'DeviceID.txt', 'w')
+    file.write(deviceID)
+    file.close()
+    return deviceID
+
+
+def checkDeviceID(_handle):
+    try:
+        file = open(profilePath + 'DeviceID.txt', 'r')
+        deviceID = file.readline()
+        if deviceID.strip() == "":
+            file.close()
+            deviceID = generateUUID(_handle)
+        else:
+            file.close()
+    except OSError:
+        deviceID = generateUUID(_handle)
+    return deviceID
+
+
+def channelFunc(channelsData):
+    channels = []
+    for elem in channelsData:
+        name = ['name', elem['name']]
+        thumb = ['thumb', elem['icon']]
+        genre = ['genre', 'TV channel']
+        id = ['id', elem['id']]
+        video = ['video', elem['id']]
+        channel = [name, thumb, video, genre, id]
+        channels.append(dict(channel))
+    return channels
+
+
+def filmFunc(filmsData):
+    films = []
+    filmsData = [filmsData]
+    for elem in filmsData:
+        name = ['name', elem['title']]
+        thumb = ['thumb', elem['img'][0]['src'] + '?w=462&h=261&crop=true']
+        genre = ['genre', ' '.join([ele['name'] for ele in elem['genres']])]
+        id = ['id', elem['id']]
+        video = ['video', elem['id']]
+        film = [name, thumb, video, genre, id]
+        films.append(dict(film))
+    return films
